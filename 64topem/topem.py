8fd196ed3d3f08269418993ac59f78027a6103bf
Raymon Skjørten Hansen <rsha@dips.no>
2022-10-11 22:29:12 +0200

    Make topem script accept argument OR take from stdin

--- 64topem/topem.py
+++ 64topem/topem.py
@@ -1,13 +1,24 @@
 import sys
+import argparse
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='Wrap a PEM certificate in header and footer\n'
+    )
+    parser.add_argument('input', nargs='?')
+    return parser.parse_args()
 
-def pre(label: str) -> str:
-    return f"-----BEGIN {label}-----\n"
+if __name__ == '__main__':
+    args = parse_args()
+    if not sys.stdin.isatty():
+        from_input = sys.stdin.read()
+    elif args.input is None:
+        sys.exit()
+    else:
+        from_input = args.input + "\n"
 
-
-def post(label: str) -> str:
-    return f"-----END {label}-----"
-
-
-base64 = "".join(sys.stdin.readlines())
-print(pre("CERTIFICATE") + base64 + post("CERTIFICATE"))
+    print(f"""
+-----BEGIN CERTIFICATE-----
+{from_input}-----END CERTIFICATE-----
+""")
