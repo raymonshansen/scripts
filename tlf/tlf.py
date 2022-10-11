746494643c17a56f8717778f0c33f8911ab285c8
Raymon Skjørten Hansen <rsha@dips.no>
2022-10-11 23:15:35 +0200

    Update tlf script to use new Gulesider API

--- tlf/tlf.py
+++ tlf/tlf.py
@@ -1,18 +1,21 @@
 import requests
 import sys
-import pprint
-from typing import Dict, Any
 
-BASE_URL = "https://www.gulesider.no"
-def result_item_to_dict(item: dict)-> Dict[str, str]:
-    d = dict()
-    d["name"] = item["name"]
-    d["street"] = f'{item["address"][0]["streetName"]} {item["address"][0]["streetNumber"]}'
-    d["area"] = f'{item["address"][0]["postCode"]} {item["address"][0]["postArea"]}'
-    d["phone_number"] = ""
-    if item["phoneNumbers"]:
-        d["phone_number"] = f'{item["phoneNumbers"][0]["phoneNumber"]}'
-    return d
+def person(item: dict) -> dict[str, str]:
+    return {
+        "name": item["name"]["firstName"] + " " + item["name"]["lastName"],
+        "street": item["addresses"][0]["streetName"] + " " + item["addresses"][0]["streetNumber"],
+        "area": item["addresses"][0]["postalCode"] + " " + item["addresses"][0]["postalArea"],
+        "phone_number": item["phones"][0]["number"]
+    }
+
+def company(item: dict) -> dict[str: str]:
+    return {
+        "name": item["name"],
+        "street": item["addresses"][0]["streetName"] + " " + item["addresses"][0]["streetNumber"],
+        "area": item["addresses"][0]["postalCode"] + " " + item["addresses"][0]["postalArea"],
+        "phone_number": item["phones"][0]["number"]
+    }
 
 # No arguments is handled py the PowerShell-script calling tlf.py
 include_num = False
@@ -29,38 +32,34 @@ elif len(sys.argv) > 2:
         print(f"Searching for {sys.argv[0]}...\n")
         search_word = sys.argv[0]
 
-query_dict = {"query": search_word, "profile": "no"}
-resp = requests.get(f"{BASE_URL}/api/search/count", params=query_dict)
-response_dict = dict(resp.json())
-
 # Determine what type of search this would require, personal or company?
-num_type = ""
-if response_dict["ps"]:
-    num_type = "ps"
-if response_dict["cs"]:
-    num_type = "cs"
+resp = requests.get(f"https://one-back.gulesider.no/custom/superSearch/gulesider/{search_word}")
+match resp.content:
+    case b"cs":
+        num_type = "companies"
+    case b"ps":
+        num_type = "persons"
+    case _:
+        num_type = None
+
 if num_type:
-    query_dict = {"query": search_word,
-                  "sortOrder": "default",
-                  "profile": "no",
-                  "page": "1",
-                  "lat": "0",
-                  "lng": "0",
-                  "limit": "25",
-                  "client": "true",
-                 }
-    resp = requests.get(f"{BASE_URL}/api/{num_type}", params=query_dict)
-    raw_search_result = dict(resp.json())
+    url = f"https://www.gulesider.no/_next/data/icrEtxkRx7HLangO6erlo/nb/search/{search_word}/{num_type}/1/0.json?query={search_word}&searchType={num_type}&page=1&id=0"
+    resp = requests.get(url)
+    raw_search_result = dict(resp.json())["pageProps"]["initialState"][num_type]
 else:
     print("Could not determine number type")
     sys.exit(1)
 
-hits = raw_search_result["hits"]
-if hits:
-    print(f"{hits} results from searching '{search_word}':\n")
-    results: Dict[str, list] = dict()
-    results["results"] = [result_item_to_dict(item) for item in raw_search_result["items"]]
-    for result in results["results"]:
+if raw_search_result:
+    print(f"{len(raw_search_result)} results from searching '{search_word}':\n")
+    match num_type:
+        case "companies":
+            results = [company(res) for res in raw_search_result]
+        case "persons":
+            results = [person(res) for res in raw_search_result]
+        case _:
+            results = []
+    for result in results:
         print(result["name"])
         print(result["street"])
         print(result["area"])
