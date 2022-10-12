912d45f39e98187fce2fe645a930b798226616c5
Mads Johansen <maxjohansen@fastmail.com>
2022-10-12 15:31:29 +0200

    Refactor for testability

--- tlf/tlf.py
+++ tlf/tlf.py
@@ -1,13 +1,10 @@
+from dataclasses import dataclass
+from typing import Tuple
 import requests
 import sys
 
 # This might change when Gulesider is updated
-BASE_URL = "https://www.gulesider.no/_next/data/icrEtxkRx7HLangO6erlo/nb/search/"
-
-
-def filter_empty(dictionary):
-    "Removes all empty values from the given dictionary"
-    return {k: v for k, v in dictionary.items() if v}
+BASE_URL = "https://www.gulesider.no/_next/data/icrEtxkRx7HLangO6erlo/nb/search"
 
 
 def from_optional(dictionary: dict, *keys: str) -> str:
@@ -15,59 +12,56 @@ def from_optional(dictionary: dict, *keys: str) -> str:
     return " ".join(filter(None, (dictionary.get(key) for key in keys)))
 
 
-def person(item: dict) -> dict[str, str]:
-    address = item["addresses"][0]
-    return {
-        "name": from_optional(item["name"], *item["name"].keys()),
-        "street": from_optional(address, "streetName", "streetNumber"),
-        "area": from_optional(address, "postalCode", "postalArea"),
-        "phone_numbers": "\n".join(
-            f"Tlf: {p['number']}" for p in item.get("phones", [])
-        ),
-    }
+@dataclass
+class SearchResult:
+    """Represents a search result from Gulesider
+
+    Most properties can be empty, but printing the result directly
+    will handle the display logic for you.
+    """
+
+    name: str
+    street: str
+    area: str
+    phone_numbers: list[str]
+
+    def __init__(self, item: dict):
+        self.name = (
+            item["name"]
+            # Companies have names as plain strings
+            if isinstance(item["name"], str)
+            # But persons have name dictionaries with optional properties
+            else from_optional(item["name"], *item["name"].keys())
+        )
+        # Some results have no addresses
+        address = item["addresses"][0] if item["addresses"] else {}
+        self.street = from_optional(address, "streetName", "streetNumber")
+        self.area = from_optional(address, "postalCode", "postalArea")
+        self.phone_numbers = [p["number"] for p in item.get("phones", [])]
+
+    def __str__(self):
+        phone_numbers = "\n".join(f"Tlf: {p}" for p in self.phone_numbers)
+        return "\n".join(
+            filter(None, [self.name, self.street, self.area, phone_numbers])
+        )
 
 
-def company(item: dict) -> dict[str, str]:
-    address = item["addresses"][0]
-    return {
-        "name": item["name"],
-        "street": from_optional(address, "streetName", "streetNumber"),
-        "area": from_optional(address, "postalCode", "postalArea"),
-        "phone_number": "\n".join(
-            f"Tlf: {p['number']}" for p in item.get("phones", [])
-        ),
-    }
-
-
-def main(search_word: str):
+def search_gulesider(query: str) -> Tuple[list[SearchResult], list[SearchResult]]:
     # Searching for companies returns persons as well, so this url works for everything
-    resp = requests.get(
-        f"{BASE_URL}{search_word}/companies/1/0.json?query={search_word}&searchType=companies&page=1&id=0"
-    )
+    resp = requests.get(f"{BASE_URL}/{query}/companies/1/0.json?query={query}")
     # The response is for a SPA built on Next.js, so we can dig
     # directly into the page props for our data
-    raw_search_result = dict(resp.json())["pageProps"]
+    page_props = dict(resp.json())["pageProps"]
 
-    # Happens for unlisted numbers
-    if "initialState" not in raw_search_result:
-        print(f"No results for '{search_word}'")
-        sys.exit(1)
+    # No results can happen for unlisted numbers
+    if "initialState" not in page_props:
+        return [], []
 
-    state = raw_search_result["initialState"]
+    state = page_props["initialState"]
+    persons = [SearchResult(res) for res in state["persons"]]
+    companies = [SearchResult(res) for res in state["companies"]]
 
-    num_hits = state["totalHits"]
-    if num_hits == 0:
-        print(f"No results for '{search_word}'")
-        sys.exit(1)
-
-    companies = [filter_empty(company(res)) for res in state["companies"]]
-    persons = [filter_empty(person(res)) for res in state["persons"]]
-
-    plural = "s" if num_hits > 1 else ""
-    print(f"{num_hits} result{plural} for '{search_word}':\n")
-
-    for result in persons + companies:
-        print("\n".join(result.values()), end="\n\n")
+    return persons, companies
 
 
 if __name__ == "__main__":
@@ -78,4 +72,15 @@ if __name__ == "__main__":
         print(f"Usage: {prog} <name or number>")
         sys.exit()
 
-    main(" ".join(args))
+    query = " ".join(args)
+    persons, companies = search_gulesider(query)
+
+    num_hits = len(persons + companies)
+    if num_hits == 0:
+        print(f"No results for '{query}'")
+        sys.exit()
+
+    plural = "s" if num_hits > 1 else ""
+    print(f"{num_hits} result{plural} for '{query}':\n")
+
+    print("\n\n".join(str(res) for res in persons + companies))
